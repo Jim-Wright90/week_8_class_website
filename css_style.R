edd1b8e42c7b6009d1b4fea1ee0eeef1d02afa9d
Jim Wright <jjwright90@gmail.com>
2020-02-26 11:20:42 -0800

    flex dashboard practice added and continued blog practice

---
+++ css_style.R
@@ -0,0 +1,8 @@
+@import url('https://fonts.googleapis.com/css?family=Trade+Winds&display=swap');
+
+.distill-site-nav {
+  color: rgba(255, 255, 255, 0.8);
+  background-color: #455a64; 
+    font-size: 15px;
+  font-weight: 300;
+}
